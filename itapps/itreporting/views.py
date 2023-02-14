bd3d801c0123ff2085b21aab766e19f2525c3536
joejoba <josepholasimbo1@gmail.com>
2023-02-14 17:56:27 +0000

    This is my Django Experiment

---
+++ itapps/itreporting/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'itreporting/home.html')
+
+def about(request):
+    return render(request, 'itreporting/about.html')
+
+def contact(request):
+    return render(request, 'itreporting/contact.html')
